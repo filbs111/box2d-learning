b2f31540b62a6ac5bdb050c1572873eb0767454c
filbs111 <ed.filby@gmail.com>
2017-05-19 04:04:15 +0100

    use shared mechanics.js for worker, non worker
    also notice that clipper.js appears to not fully work within worker #24

--- js/worker.js
+++ js/worker.js
@@ -1,26 +1,17 @@
 importScripts('../lib/Box2d.min.js',
 			'../lib/clipper.js',
-			'../js-utils/gaussrand.js');
+			'../js-utils/gaussrand.js',
+			'../js/mechanics.js');
 
-var   b2Vec2 = Box2D.Common.Math.b2Vec2
-		, b2Mat22 = Box2D.Common.Math.b2Mat22
-        , b2BodyDef = Box2D.Dynamics.b2BodyDef
-        , b2Body = Box2D.Dynamics.b2Body
-        , b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-        , b2Fixture = Box2D.Dynamics.b2Fixture
-        , b2World = Box2D.Dynamics.b2World
-		, b2WorldManifold = Box2D.Collision.b2WorldManifold
-        , b2MassData = Box2D.Collision.Shapes.b2MassData
-        , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-        , b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
-		, b2Shape = Box2D.Collision.Shapes.b2Shape
-        , b2DebugDraw = Box2D.Dynamics.b2DebugDraw
-	    , b2BuoyancyController = Box2D.Dynamics.Controllers.b2BuoyancyController
-          ;
 
-var cpr = new ClipperLib.Clipper();
-		  
+
 		  
 self.onmessage = function(e) {
 	postMessage("received message from main : " + e.data);
 };
+
+
+
+init();
+
+
